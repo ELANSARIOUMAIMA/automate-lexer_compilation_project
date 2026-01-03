ccaa740ae5ed41af01ac49561cedc30113591228
AYAESOUBAI <ayaesoubai@gmail.com>
2026-01-03 14:36:42 +0100

    Implement semantic analysis in main parser
    Added semantic analysis check before displaying the automate.

--- main_parser.c
+++ main_parser.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "parser.h"
 #include "automate.h"
+#include "semantic.h"
+
+
 
 int main() {
     printf("\n");
@@ -9,12 +12,19 @@ int main() {
     // Parser le fichier
     Automate* aut = parser("automate.txt");
     
+    // Analyse Sémantique
+    if (!check_semantic(aut)) {
+        printf("Compilation arrêtée à cause d’erreurs sémantiques.\n");
+        return 1;
+    }
+    
     // Afficher l'automate construit
     afficher_automate(aut);
     
     // Liberer la memoire
     liberer_automate(aut);
-    
+
     printf("Programme termine avec succes!\n\n");
     return 0;
+    
 }
